eeeccbea63e11b7e7729b5c581155bd65ebd299c
edawg <muhammadridho5654@gmail.com>
2026-06-21 18:05:18 +0700

    chore: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css";
 import "./i18n";
 
 // Strict mode dipertahankan untuk dev — tidak ada efek di production build
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
+    <Analytics />
     <App />
   </HelmetProvider>
 );
